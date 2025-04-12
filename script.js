84b9801bb1891ad36470dea68308db35b3693fb8
Jasmine Sun <jasminesun@MacBook-Pro-4.local>
2025-04-12 02:26:21 -0700

    Update text content

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const timelineData = [
             text: '"Lecture to the London Mathematical Society" from the Collected Works of Alan M. Turing, Volume 1: Mechanical Intelligence',
             link: null
         },
-        context: "First known concept of a machine intelligent enough to complete human-like tasks, learn from experience, etc. Also discusses automation, training, alignment, hallucination.",
+        context: "This was the first known conception of an electronic computer intelligent enough to complete human-like tasks, learn from experience, etc. The lecture also discusses issues like labor revolts, alignment problems, and hallucination.",
         tags: ["processes", "performance"]
     },
     {
@@ -21,7 +21,7 @@ const timelineData = [
             text: "Proposal for the Dartmouth Summer Research Project on AI",
             link: "http://jmc.stanford.edu/articles/dartmouth/dartmouth.pdf"
         },
-        context: "First published usage of \"artificial intelligence\" / invention of the field",
+        context: "This was the first published usage of \"artificial intelligence\" and is credited with the invention of the AI field.",
         tags: ["processes", "performance"]
     },
     {
@@ -33,7 +33,7 @@ const timelineData = [
             text: "Minds, brains, and programs",
             link: "https://home.csulb.edu/~cwallis/382/readings/482/searle.minds.brains.programs.bbs.1980.pdf"
         },
-        context: "Searle's \"Strong AI\" hypothesis, which he refutes, suggests that a computer program simulating a mind is also conscious and has its own mental states. (This is not how most AI researchers or theorists would define AI.) (Searle is the \"Chinese room\" guy)",
+        context: "Searle refutes the strong AI hypothesis with the Chinese Room thought experiment.",
         tags: ["processes"]
     },
     {
@@ -45,7 +45,7 @@ const timelineData = [
             text: "Nanotechnology and National Security",
             link: "https://web.archive.org/web/20100612234609/https://foresight.org/Conferences/MNT05/Papers/Gubrud/index.html"
         },
-        context: "First recorded usage of AGI but went unnoticed by AI community",
+        context: "This was the first recorded usage of AGI, but went mostly unnoticed by the AI community.",
         tags: ["generality", "performance"]
     },
     {
@@ -54,10 +54,10 @@ const timelineData = [
         term: "AGI",
         quote: "What is meant by <strong>AGI</strong> is, loosely speaking, AI systems that possess a reasonable degree of self-understanding and autonomous self-control, and have the ability to solve a variety of complex problems in a variety of contexts, and to learn to solve new problems that they didn't know about at the time of their creation. A marked distinction exists between practical AGI work and, on the other hand:<br><br>• Pragmatic but specialized \"narrow AI\" research which is aimed at creating programs carrying out specific tasks like playing chess, diagnosing diseases, driving cars and so forth (most contemporary AI work falls into this category.)<br>• Purely theoretical AI research, which is aimed at clarifying issues regarding the nature of intelligence and cognition, but doesn't involve technical details regarding actually realizing artificially intelligent software.",        
         source: {
-            text: "Goertzel's book (pub. 2005) needed a title. He didn't like \"strong AI,\" and Shane came up with \"AGI.\"",
+            text: "Artificial General Intelligence (2005)",
             link: "https://goertzel.org/who-coined-the-term-agi"
         },
-        context: "Goertzel and Legg are credited with popular diffusion of the term AGI.",
+        context: "Goertzel and Legg are credited with the popular diffusion of the term AGI. This became the title of Goertzel's 2005 anthology of AGI research papers.",
         tags: ["processes", "generality", "performance"]
     },
     {
@@ -66,7 +66,7 @@ const timelineData = [
         term: "strong AI",
         quote: "Artificial intelligence permeates our economy. It's what I define as \"narrow\" AI: machine intelligence that equals or exceeds human intelligence for specific tasks… So what are the prospects for <strong>\"strong\" AI</strong>, which I describe as machine intelligence with the full range of human intelligence?",
         source: {
-            text: "Long Live AI / The Singularity Is Near",
+            text: "Long Live AI",
             link: "https://www.forbes.com/forbes/2005/0815/030.html"
         },
         context: null,
@@ -76,12 +76,11 @@ const timelineData = [
         year: 2005,
         author: "Nils Nilsson",
         term: "human-level intelligence",
-        quote: "Machines exhibiting true <strong>human-level intelligence</strong> should be able to do many of the things humans are able to do. Among these activities are the tasks or \"jobs\" at which people are employed. I suggest we replace the Turing test by something I will call the \"employment test.\" To pass the employment test, AI programs must be able to perform the jobs ordinarily performed by humans. Progress toward human-level AI could then be measured by the fraction of these jobs that can be acceptably performed by machines. Rather than work toward this goal of automation by building special-purpose systems, I argue for the development of general-purpose, educable systems that can learn and be taught to perform any of the thousands of jobs that humans can perform.",
+        quote: "Machines exhibiting true <strong>human-level intelligence</strong> should be able to do many of the things humans are able to do. Among these activities are the tasks or \"jobs\" at which people are employed. I suggest we replace the Turing test by something I will call the \"employment test.\"<br><br> To pass the employment test, AI programs must be able to perform the jobs ordinarily performed by humans. Progress toward human-level AI could then be measured by the fraction of these jobs that can be acceptably performed by machines. Rather than work toward this goal of automation by building special-purpose systems, I argue for the development of general-purpose, educable systems that can learn and be taught to perform any of the thousands of jobs that humans can perform.",
         source: {
             text: "Human-Level Artificial Intelligence? Be Serious!",
             link: "https://ai.stanford.edu/~nilsson/OnlinePubs-Nils/General%20Essays/AIMag26-04-HLAI.pdf"
         },
-        context: "Nilsson says \"general-purpose, educable\" systems are the right approach to achieve HLMI. Proposes an \"employment test\" to measure AI capabilities",
         tags: ["performance", "impacts"]
     },
     {
@@ -95,7 +94,7 @@ const timelineData = [
             position: "afterQuote"
         },
         source: {
-            text: "First formal definition of \"universal intelligence\"",
+            text: "Universal Intelligence: A Definition of Machine Intelligence",
             link: "https://arxiv.org/pdf/0712.3329"
         },
         context: null,
@@ -116,13 +115,13 @@ const timelineData = [
     {
         year: 2014,
         author: "Vincent C. Müller & Nick Bostrom",
-        term: "high-level machine intelligence (HLMI)",
+        term: "high-level machine intelligence",
         quote: "Define a <strong>'high–level machine intelligence' (HLMI)</strong> as one that can carry out most human professions at least as well as a typical human. We need to avoid using terms that are already in circulation and would thus associate the questionnaire with certain groups or opinions, like \"artificial intelligence\", \"singularity\", \"artificial general intelligence\" or \"cognitive system\". For these reasons, we settled for a definition that a) is based on behavioral ability, b) avoids the notion of a general 'human–level' and c) uses a newly coined term.",
         source: {
             text: "Future Progress in Artificial Intelligence: A Survey of Expert Opinion",
             link: "https://nickbostrom.com/papers/survey.pdf"
         },
-        context: "2014 survey of AI expert opinion — HLMI is used to be debiasing",
+        context: "A 2014 survey of AI expert opinion, where \"HLMI\" is used to be debiasing.",
         tags: ["performance"]
     },
     {
@@ -131,7 +130,7 @@ const timelineData = [
         term: "superintelligence",
         quote: "<strong>Superintelligence</strong> is defined as \"any intellect that greatly exceeds the cognitive performance of humans in virtually all domains of interest.\"",
         source: {
-            text: "From Superintelligence book",
+            text: "Superintelligence (2014)",
             link: null
         },
         context: null,
@@ -143,7 +142,7 @@ const timelineData = [
         term: "transformative AI",
         quote: "Roughly and conceptually, <strong>transformative AI</strong> refers to potential future AI that precipitates a transition comparable to (or more significant than) the agricultural or industrial revolution. I also provide (below) a more detailed definition. The concept of \"transformative AI\" has some overlap with concepts put forth by others, such as \"superintelligence\" and \"artificial general intelligence.\" However, \"transformative AI\" is intended to be a more inclusive term, leaving open the possibility of AI systems that count as \"transformative\" despite lacking many abilities humans have.",
         source: {
-            text: "Some Background on Our Views Regarding Advanced Artificial Intelligence | Open Philanthropy",
+            text: "Some Background on Our Views Regarding Advanced Artificial Intelligence",
             link: "https://www.openphilanthropy.org/research/some-background-on-our-views-regarding-advanced-artificial-intelligence/"
         },
         context: null,
@@ -155,10 +154,10 @@ const timelineData = [
         term: "high-level machine intelligence",
         quote: "<strong>\"High-level machine intelligence\"</strong> (HLMI) is achieved when unaided machines can accomplish every task better and more cheaply than human workers.",
         source: {
-            text: "Regular survey of AI experts on views on AGI, first in 2016",
+            text: "When Will AI Exceed Human Performance? Evidence from AI Experts",
             link: "https://arxiv.org/abs/1705.08807"
         },
-        context: null,
+        context: "Grace's survey of AI experts on their views on AGI, first done in 2016.",
         tags: ["performance", "impacts"]
     },
     {
@@ -167,7 +166,7 @@ const timelineData = [
         term: "AGI",
         quote: "OpenAI's mission is to ensure that <strong>artificial general intelligence (AGI)</strong> — by which we mean highly autonomous systems that outperform humans at most economically valuable work — benefits all of humanity.",
         source: {
-            text: "Defined in the OpenAI charter",
+            text: "OpenAI Charter",
             link: "https://web.archive.org/web/20180409161852/https://blog.openai.com/openai-charter/"
         },
         context: null,
@@ -179,10 +178,10 @@ const timelineData = [
         term: "general intelligence",
         quote: "The <strong>intelligence</strong> of a system is a measure of its skill-acquisition efficiency over a scope of tasks, with respect to priors, experience, and generalization difficulty. <strong>\"General intelligence\"</strong> is not a binary property which a system either possesses or lacks. It is a spectrum, tied to 1) a scope of application, which may be more or less broad, and 2) the degree of efficiency with which the system translate its priors and experience into new skills over the scope considered, 3) the degree of generalization difficulty represented by different points in the scope considered. It is conceptually unsound to set \"artificial general intelligence\" in an absolute sense (i.e. \"universal intelligence\") as a goal. The consensus definition of AGI, \"a system that can automate the majority of economically valuable work,\" while a useful goal, is an incorrect measure of intelligence.",
         source: {
-            text: "On the Measure of Intelligence / What is ARC-AGI?",
+            text: "On the Measure of Intelligence",
             link: "https://arxiv.org/pdf/1911.01547"
         },
-        context: "Chollet has been a critic of other definitions of AGI, arguing that they measure memorization rather than generality / new-skill acquisition",
+        context: "Chollet has criticized other definitions of AGI, arguing that they measure memorization rather than true generality.",
         tags: ["generality"]
     },
     {
@@ -191,7 +190,7 @@ const timelineData = [
         term: "human-level AI",
         quote: "I think the phrase AGI should be retired and replaced by \"<strong>human-level AI</strong>\". There is no such thing as <strong>AGI</strong>. Even human intelligence is very specialized. We do not realize that human intelligence is specialized because all the intelligent tasks we can think of are task that we can apprehend. But that is a tiny subset of all tasks. The overwhelming majority of tasks are completely out of reach of un-augmented human intelligence.",
         source: {
-            text: "LinkedIn post",
+            text: "LinkedIn",
             link: "https://www.linkedin.com/posts/yann-lecun_i-think-the-phrase-agi-should-be-retired-activity-6889610518529613824-gl2F"
         },
         context: null,
@@ -203,7 +202,7 @@ const timelineData = [
         term: "AGI",
         quote: "We use <strong>AGI</strong> to refer to systems that demonstrate broad capabilities of intelligence, including reasoning, planning, and the ability to learn from experience, and with these capabilities at or above human-level.",
         source: {
-            text: "Sparks of AGI paper",
+            text: "Sparks of Artificial General Intelligence: Early experiments with GPT-4",
             link: "https://arxiv.org/abs/2303.12712"
         },
         context: null,
@@ -212,7 +211,7 @@ const timelineData = [
     {
         year: 2024,
         author: "Meredith Morris et al (DeepMind)",
-        term: "Levels of AGI",
+        term: "levels of AGI",
         quote: "<strong>Artificial General Intelligence (AGI)</strong> is an important and sometimes controversial concept in computing research, used to describe an AI system that is at least as capable as a human at most tasks.",
         image: {
             url: "images/deepmind_agi_levels.png",
@@ -223,13 +222,13 @@ const timelineData = [
             text: "Levels of AGI for Operationalizing Progress on the Path to AGI",
             link: "https://arxiv.org/pdf/2311.02462"
         },
-        context: "Emphasizes generality and performance—both exceeding human performance on tasks, and adapting to new tasks via learning",
+        context: "This paper emphasizes both generality and performance. They create a separate spectrum for levels of autonomy, associated with risk.",
         tags: ["generality", "performance"]
     },
     {
         year: 2024,
         author: "OpenAI",
-        term: "Stages of AGI",
+        term: "stages of AGI",
         quote: "OpenAI has internally defined five \"stages\" of AGI",
         image: {
             url: "images/openai_agi_levels.png",
@@ -240,7 +239,7 @@ const timelineData = [
             text: "Bloomberg: OpenAI Sets Levels to Track Progress Toward Superintelligent AI",
             link: "https://www.bloomberg.com/news/articles/2024-07-11/openai-sets-levels-to-track-progress-toward-superintelligent-ai"
         },
-        context: "(In a 2023 blog post, Sam Altman similarly distinguishes between \"initial AGI\" and \"successor systems\")",
+        context: "(In a 2023 blog post, Sam Altman distinguishes between \"initial AGI\" and \"successor systems.\")",
         tags: ["performance", "impacts"]
     },
     {
@@ -264,7 +263,7 @@ const timelineData = [
             text: "Three Observations",
             link: "https://blog.samaltman.com/three-observations"
         },
-        context: "Sam blog post is notably different from the OpenAI site definition, \"highly autonomous systems that outperform humans at most economically valuable work\" \"At human level\" and \"Many fields\" also seem quite vague!",
+        context: "Sam's blog post definition is notably vaguer and weaker from the OpenAI 2018 charter's definition, \"highly autonomous systems that outperform humans at most economically valuable work\"",
         tags: ["performance"]
     }
 ];
